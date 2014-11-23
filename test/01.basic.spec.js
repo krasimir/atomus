c947731e728427a00d79bda98ae815ca55fce698
Krasimir <krasimir@outset.ws>
2014-11-23 18:21:28 +0200

    Translating the tests to the new API

--- test/01.basic.spec.js
+++ test/01.basic.spec.js
@@ -11,10 +11,7 @@ suite('Basics', function() {
 
   test('adding initial html', function(done) {
     var atomus = require('../lib');
-    var b = atomus.browser({
-      html: '<h1>Hello atomus</h1>'
-    });
-    b.ready(function(errors, window) {
+    var b = atomus.browser().html('<h1>Hello atomus</h1>').ready(function(errors, window) {
       assert.equal(b.$('body').innerHTML, '<h1>Hello atomus</h1>');
       done();
     });
@@ -22,10 +19,7 @@ suite('Basics', function() {
 
   test('adding external libs', function(done) {
     var atomus = require('../lib');
-    var b = atomus.browser({
-      scripts: [__dirname + '/data/library.js']
-    });
-    b.ready(function(errors, window) {
+    var b = atomus.browser().external(__dirname + '/data/library.js').ready(function(errors, window) {
       assert.equal(b.window.AwesomeLibrary.answer(), 42);
       done();
     });
--- test/02.events.spec.js
+++ test/02.events.spec.js
@@ -3,10 +3,7 @@ suite('Events', function() {
 
   test('Attach event listener + triggering', function(done) {
     var atomus = require('../lib');
-    var b = atomus.browser({
-      html: '<input type="text" />'
-    });
-    b.ready(function(errors, window) {
+    var b = atomus.browser().html('<input type="text" />').ready(function(errors, window) {
       var input = b.$('input');
       input.value = 'hello world';
       b.on('change', function(event) {
