f135dc5a136d8f19c12d6a364797d0bb117f2903
Krasimir Tsonev <krasimir@outset.ws>
2015-03-01 11:25:24 +0200

    Adding a test for mock as a function

--- test/04.xmlhttprequest.spec.js
+++ test/04.xmlhttprequest.spec.js
@@ -92,4 +92,35 @@ suite('XMLHTTPRequest', function() {
     });
   });
 
+  test('Runtime XHR mock', function(done) {
+    // var mockups = { 
+    //   url: '/api/method/action',
+    //   response: {
+    //     status: 200,
+    //     responseText: JSON.stringify({"id": "AAA"})
+    //   }
+    // };
+    var mock = function(params) {
+      if(params.url === '/api/method/action' && params.method === 'GET') {
+      return {
+          status: 200,
+          responseText: JSON.stringify({"id": "AAA"})
+        }
+      }
+    };
+    var atomus = require('../lib');
+    var b = atomus()
+    .external(__dirname + '/data/ajaxwrapper.js')
+    .ready(function(errors, window) {
+      b.addXHRMock(mock)
+      window.AjaxWrapper().request({
+        url: '/api/method/action',
+        json: true
+      }).done(function(result) {
+        assert(result.id, 'AAA');
+        done();
+      })
+    });
+  });
+
 });
