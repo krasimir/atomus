f6139b6776d8c1b63e1820b3c175a276bf432d3c
Krasimir Tsonev <krasimir@outset.ws>
2016-02-06 17:09:46 +0200

    Amending test suite because jsdom's ajax wrapper doesn't work properly with file protocol

--- test/04.xmlhttprequest.spec.js
+++ test/04.xmlhttprequest.spec.js
@@ -1,25 +1,6 @@
 var assert = require('assert');
 suite('XMLHTTPRequest', function() {
 
-  test('making HTTP request', function(done) {
-    this.timeout(5000);
-    var atomus = require('../lib');
-    var b = atomus()
-    .external(__dirname + '/data/ajaxwrapper.js')
-    .ready(function(errors, window) {
-      window.AjaxWrapper().request({
-        url: __dirname + '/../package.json',
-        json: true
-      }).done(function(result) {
-        assert.equal('jsdom' in result.dependencies, true);
-        done();
-      }).fail(function() {
-        assert.equal('HTTP request failed', false);
-        done();
-      });
-    });
-  });
-
   test('faking HTTP request', function(done) {
     var mockups = { 
       url: '/api/method/action',
